d4b063b9d6c739c48b4a7757353d5d621a9a11f3
wli189 <brianleelwt@gmail.com>
2024-09-23 18:02:01 -0500

    feat: player look control

---
+++ Assets/Scripts/MosueLook.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MosueLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    void Start()
+    {
+        // Lock the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Controls xAxis of mouse(left and right)
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // Controls yAxis of mouse(up and down)
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Controls "nodding" of player
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Controls looking left and right of player
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
