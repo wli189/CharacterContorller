76b084f6dabb43d4d33d8234cff6a3620dbe1e6a
wli189 <brianleelwt@gmail.com>
2024-09-23 18:59:50 -0500

    feat: player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,14 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.8f;
+
+    public Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+    public float jumpHeight = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +26,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 direction = transform.right * x + transform.forward * z;
 
         controller.Move(direction * speed * Time.deltaTime);
+
+        velocity.y += 1.5f * gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
