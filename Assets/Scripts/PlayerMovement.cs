610767c1211d239b7b9ec073821be389e344c5c6
wli189 <brianleelwt@gmail.com>
2024-09-23 18:29:00 -0500

    fix: player move speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
+
+    public float speed = 12f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +21,8 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 direction = transform.right * x + transform.forward * z;
 
-        controller.Move(move);
+        controller.Move(direction * speed * Time.deltaTime);
     }
 }
